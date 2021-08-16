b50552a2873b5e3038bbb1fc74679981624a28d4
andreacerasani <andrea.cerasani@mail.polimi.it>
2021-08-16 23:42:05 +0200

    Changed i_clk'event and i_clk = '1' to rising_edge(i_clk)

--- project_reti_logiche.srcs/sources_1/new/project_reti_logiche.vhd
+++ project_reti_logiche.srcs/sources_1/new/project_reti_logiche.vhd
@@ -18,57 +18,6 @@
 -- 
 ----------------------------------------------------------------------------------
 
-
-library IEEE;
-library work;
-use IEEE.STD_LOGIC_1164.ALL;
-
--- Uncomment the following library declaration if using
--- arithmetic functions with Signed or Unsigned values
-use IEEE.NUMERIC_STD.ALL;
-
--- Uncomment the following library declaration if instantiating
--- any Xilinx leaf cells in this code.
---library UNISIM;
---use UNISIM.VComponents.all;
-
---Synchronous down 8-bit counter with asynchronous reset
-entity down_counter is
-	port (
-		i_clk : in std_logic;
-		i_rst : in std_logic;
-		syn_rst : in std_logic;
-		i_load : in std_logic;
-		i_en : in std_logic; --if 1 counter is decreased
-		i_data : in std_logic_vector(7 downto 0);
-		o_zero : out std_logic
-		);
-end down_counter;
-
-architecture Behavioral of down_counter is
-	signal r_reg : unsigned(7 downto 0); -- internal counter signal
-	signal r_next: unsigned(7 downto 0);
-begin
---register
-	process(i_clk, i_rst)
-	begin
-		if (i_rst = '1') or (rising_edge(i_clk) and (syn_rst = '1')) then 
-			r_reg <= (others => '0'); --clear
-		elsif (rising_edge(i_clk)) then
-			r_reg <= r_next;
-		end if;
-	end process;
-	
---next-state logic
-	r_next <= unsigned(i_data) when i_load = '1' else
-			r_reg - 1 when (i_en = '1' and not(r_reg = 0))else
-			r_reg;
-			
---output logic
-	o_zero <= '1' when r_reg = 0 else '0';
-end Behavioral;
-
-
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
 use IEEE.NUMERIC_STD.ALL;
@@ -148,7 +97,7 @@ begin
     begin
         if(i_rst = '1') then
             r_max <= "00000000";
-        elsif i_clk'event and i_clk = '1' then
+        elsif (rising_edge(i_clk)) then
             if (syn_rst = '1') then
                 r_max <= "00000000";
             elsif(i_en = '1') then
@@ -162,7 +111,7 @@ begin
     begin
         if(i_rst = '1') then
             r_min <= "11111111";
-        elsif i_clk'event and i_clk = '1' then
+        elsif (rising_edge(i_clk)) then
             if (syn_rst = '1') then
                 r_min <= "11111111";
             elsif(i_en = '1') then
@@ -356,7 +305,7 @@ begin
     begin
         if(i_rst = '1') then
             col_reg <= "00000000";
-        elsif i_clk'event and i_clk = '1' then
+        elsif (rising_edge(i_clk)) then
             if (syn_rst = '1') then
                 col_reg <= "00000000";
             elsif(set_col = '1') then
@@ -370,7 +319,7 @@ begin
     begin
         if(i_rst = '1') then
             row_reg <= "00000000";
-        elsif i_clk'event and i_clk = '1' then
+        elsif (rising_edge(i_clk)) then
             if (syn_rst = '1') then
                 row_reg <= "00000000";
             elsif(set_row = '1') then
@@ -529,7 +478,7 @@ begin
     begin
         if(i_rst = '1') then
             cur_state <= START;
-        elsif i_clk'event and i_clk = '1' then
+        elsif (rising_edge(i_clk)) then
             cur_state <= next_state;
         end if;
     end process;
